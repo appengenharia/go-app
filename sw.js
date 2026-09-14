ea50243ad5736c579868fe95aa9d8587d388eec1
Josimar <josimar.rrocha@gmail.com>
2026-09-14 17:41:50 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,34 +1,41 @@
-// CFO - Service Worker v3 - Network First const CACHE = "cfo-v3"; 
+// GO - Service Worker v4 - Network First
+const CACHE = "go-v4";
 
-self.addEventListener("install", e => { 
-self.skipWaiting(); 
-}); 
+self.addEventListener("install", e => {
+  self.skipWaiting();
+});
 
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(k =>
+            k.startsWith("cfo-") ||
+            k.startsWith("go-")
+          )
+          .filter(k => k !== CACHE)
+          .map(k => caches.delete(k))
+      )
+    )
+  );
 
-self.addEventListener("activate", e =>{ 
-e.waitUntil( 
-caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))) 
+  self.clients.claim();
+});
 
-) 
-); 
+self.addEventListener("fetch", e => {
+  if(e.request.method !== "GET") return;
 
-self.clients.claim(); 
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
 
-}); 
+        caches.open(CACHE)
+          .then(c => c.put(e.request, clone));
 
-
-self.addEventListener("fetch", e => { 
-if(e.request.method !== "GET") return; 
-e.respondWith( 
-fetch(e.request) 
-.then(res => { 
-const clone = res.clone();); 
-caches.open(CACHE).then(c => c.put(e.request, clone)); 
-return res; 
-
-}) 
-
-.catch(() => caches.match(e.request)) 
-
-); 
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
 });
